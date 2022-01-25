e528ee5eb0bb0e9de04b486cb1d12dae0180bb8c
Brian <brianmweinstein@gmail.com>
2022-01-25 16:09:47 -0500

    Validation to Models

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -1,7 +1,17 @@
+# == Schema Information
+#
+# Table name: shortened_urls
+#
+#  id         :integer          not null, primary key
+#  long_url   :string           not null
+#  short_url  :string           not null
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class ShortenedUrl < ApplicationRecord
     validates :long_url, presence: true
     validates :short_url, presence: true, uniqueness: true
-    validates user_id: true, presence: true
-
-    
+    validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@
 # Table name: users
 #
 #  id         :integer          not null, primary key
-#  email      :string
+#  email      :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
