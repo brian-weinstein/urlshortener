4e934da130d49277d81f28a148663c9aa7338c1a
Brian <brianmweinstein@gmail.com>
2022-01-25 16:23:46 -0500

    Added random, unique URL shortening

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -14,4 +14,22 @@ class ShortenedUrl < ApplicationRecord
     validates :long_url, presence: true
     validates :short_url, presence: true, uniqueness: true
     validates :user_id, presence: true
+
+    belongs_to :submitter,
+        primary_key: :id,
+        foreign_key: :user_id
+        class_name: :User
+
+    def self.random_code
+        rand = SecureRandom.urlsafe_base64(16)
+        return random_code unless ShortenedUrl.exists?(short_url: random_code)
+    end
+
+    def self.create_with_user_and_long_url!(user,long_url)
+        ShortenedUrl.create!(
+            user_id: user.id,
+            long_url: long_url,
+            short_url: ShortenedUrl.random_code
+        )
+    end
 end
